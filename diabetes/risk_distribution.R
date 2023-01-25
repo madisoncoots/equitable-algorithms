f0d5b5ea346c92c943636418590341d3a4eba588
Madison Coots <madisoncoots@dhcp-10-250-61-136.harvard.edu>
2023-01-25 11:32:36 -0500

    risk distribution with shading for white only

--- diabetes/risk_distribution.R
+++ diabetes/risk_distribution.R
@@ -1,5 +1,6 @@
 library(tidyverse)
- 
+library(scales)
+
 data <- readRDS("/Users/madisoncoots/Documents/harvard/research/equitable-algorithms/data/data.rds")
 
 formula <- diabetes ~ race + ridageyr + bmxbmi
@@ -26,27 +27,60 @@ marginal_plot_data <- data_with_pred %>%
   # filter(race == "Asian American" | race == "White American") %>%
   mutate(density = "marginal")
 
-# marginal_plot_data %>%
-#   ggplot(aes(x=risk_score, color=race)) +
-#   geom_density(aes(weight=round(wtmec8yr/1000))) + # !!!! NOTE !!!! bw makes a pretty big diff here, why???
-#   geom_vline(data = diabetes_incidence_asian, aes(xintercept = incidence, 
-#                                                   color = race),
-#              linetype = "dashed") +
-#   geom_vline(data = diabetes_incidence_white, aes(xintercept = incidence, color = race),
-#              linetype = "dashed") +
-#   geom_vline(xintercept = 0.015) +
-#   xlab("Probability of having diabetes") +
-#   ylab("Density") + 
-#   scale_x_continuous(labels = scales::percent,
-#                      expand = c(0,0)) +
-#   theme_bw() +
-#   theme(legend.position = c(.8, .82),
-#         legend.title = element_blank(),
-#         axis.ticks.y=element_blank(),
-#         axis.text.y=element_blank()) +
-#   scale_color_brewer(palette = "Set2") +
-#   coord_cartesian(xlim = c(0, 0.2))
-# 
+# Experimenting with marginal plot data only
+# ------------------------------------------------------------------------------
+
+white_risk_score <- marginal_plot_data %>%
+  filter(race == "White American") %>%
+  select(risk_score, wtmec8yr)
+
+density()
+
+white_density <- density(white_risk_score$risk_score,
+                         weights = white_risk_score$wtmec8yr/sum(white_risk_score$wtmec8yr),
+                         n = 1024)
+
+white_density_df <- data.frame(
+  x = white_density$x,
+  y = white_density$y,
+  race = "White American"
+) %>%
+  mutate(y = if_else(x > 0.015, y, 0)) %>%
+  bind_rows(data.frame(x = 0.015, y = 0, race = "White American")) %>%
+  arrange(x, y)
+
+marginal_plot_data %>%
+  ggplot(aes(x=risk_score)) +
+  geom_polygon(data = white_density_df, aes(x = x, y = y, fill = race),
+  alpha = 0.2) +
+  geom_density(aes(weight=round(wtmec8yr/1000), color = race)) + # !!!! NOTE !!!! bw makes a pretty big diff here, why???
+  # geom_density(aes(weight=round(wtmec8yr/1000), fill = race, color = race), alpha = 0.2) +
+  # geom_vline(data = line_annotations, aes(xintercept = incidence, color = race),
+  #            linetype = "dashed") +
+  geom_vline(xintercept = 0.015) +
+  # geom_hline(yintercept = 0) +
+  coord_cartesian(xlim = c(0, 0.2), expand = FALSE, y = c(0, 11)) +
+  xlab("Probability of having diabetes") +
+  ylab("Density") +
+  scale_x_continuous(labels = scales::percent,
+                     expand = c(0,0)) +
+  theme_bw() +
+  theme(legend.position = c(.8, .82),
+        legend.title = element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.y=element_blank()) +
+  scale_color_brewer(palette = "Set2") +
+  scale_fill_brewer(palette = "Set2") +
+  
+  guides(fill = "none")  # removes fill from legend
+
+
+dpb <- ggplot_build(density_plot)
+
+# ------------------------------------------------------------------------------
+
+
+ 
 conditional_plot_data <- data_with_pred %>%
   # filter(race == "Asian American" | race == "White American") %>%
   filter(!diabetes) %>%
@@ -93,6 +127,8 @@ plot_data %>%
         plot.margin = margin(10, 10, 10, 10, "pt")) +
   scale_color_brewer(palette = "Set2") +
   coord_cartesian(xlim = c(0, .2))
+
+density(plot_data$risk_score)$x
 
 # Attempt at using freq poly instead -------------------------------------------
 #  -----------------------------------------------------------------------------
